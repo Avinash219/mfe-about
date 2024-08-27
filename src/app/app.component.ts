96f3b563bf56627b4a1c64d0845db12fcfa43516
Avinash219 <avinash.dev21987@gmail.com>
2024-08-27 09:46:26 +0530

    Shared service integration with shell app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { SharedMessageBusService } from 'shared-message-bus';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, AsyncPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'about';
+  messageBus = inject(SharedMessageBusService);
+  userAddress$ = new BehaviorSubject<string>('');
+
+  ngOnInit() {
+    this.messageBus.subscribe('address', this.showUserDetails.bind(this));
+  }
+
+  showUserDetails(data: any) {
+    this.userAddress$.next(data);
+  }
 }
